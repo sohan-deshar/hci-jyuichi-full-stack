a450bcd7d8656b58b304215d2ddc74d4e52278d2
Sohan Deshar <me.sohandeshar@gmail.com>
2022-04-03 14:17:18 +0200

    Added initial configs for mongo and docker

---
+++ src/main/java/api/mongo/juichibackend/JuichiBackendApplication.java
@@ -0,0 +1,13 @@
+package api.mongo.juichibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuichiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuichiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/mongo/juichibackend/JuichiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.mongo.juichibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuichiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
